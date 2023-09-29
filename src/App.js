30e34b67b12a8bcbca4360e89e9ab45f5f75082f
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2023-09-29 19:33:51 +1000

    question and button done!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, { Component } from 'react';
 import {FaHeart} from 'react-icons/fa';
+import dog from "./dog.svg";
+import baby from "./baby.svg";
 
 
 function App() {
@@ -9,10 +11,12 @@ function App() {
       <header className="App-header" id="App-header">
         <h1 class= "welcom" id="welcom">Welcome <FaHeart style={{color: 'pink'}}/></h1> 
       </header>
-      <div>
-        <h3 class="owner_question" id="owner_question">What are you looking after?</h3>
-        <button>Human</button>
-        <button>Dog</button>
+      <div class="app_question">
+        <p class="owner_question" id="owner_question">What are you looking after with this application?</p>
+        <div class="button_section">
+          <button class="baby_button"><img class="baby_img"src={baby}/>Human</button>
+          <button class="dog_button"><img class="dog_img"src={dog}/>Dog</button>
+        </div>
       </div>
     </div>
 
