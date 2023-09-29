9acfd4a58dcd291edbc37cfa3bc206c4f7ab0377
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2023-09-29 16:17:43 +1000

    welcom done!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-import { FaHeart} from 'react-icons/fa';
+import {FaHeart} from 'react-icons/fa';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome <FaHeart style={{color: 'pink'}}/></h1> 
+    <div className="App" id="App">
+      <header className="App-header" id="App-header">
+        <h1 class= "welcom" id="welcom">Welcome <FaHeart style={{color: 'pink'}}/></h1> 
       </header>
+      <div>
+        <h3 class="owner_question" id="owner_question">What are you looking after?</h3>
+        <button>Human</button>
+        <button>Dog</button>
+      </div>
     </div>
+
   );
 }
 
