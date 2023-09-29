dc98f2123363b06c9d88652773cf5942aa5bd552
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2023-09-29 22:05:20 +1000

    doesnt work yet

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,20 +3,23 @@ import React, { Component } from 'react';
 import {FaHeart} from 'react-icons/fa';
 import dog from "./dog.svg";
 import baby from "./baby.svg";
+import {Link} from "react-router-dom";
 
 const Welcome = () => {
-  <div className="App" id="App">
-    <header className="App-header" id="App-header">
-      <h1 class= "welcom" id="welcom">Welcome <FaHeart style={{color: 'pink'}}/></h1> 
-    </header>
-    <div class="app_question" id="app_question">
-      <p class="owner_question" id="owner_question">What are you looking after with this application?</p>
-      <div class="button_section">
-        <Link  to="/register_form"><button class="baby_button"><img class="baby_img"src={baby}/>Human</button></Link>
-        <Link  to="/register_form"><button class="dog_button"><img class="dog_img"src={dog}/>Dog</button></Link>
+  return (
+    <div className="App" id="App">
+      <header className="App-header" id="App-header">
+        <h1 class= "welcom" id="welcom">Welcome <FaHeart style={{color: 'pink'}}/></h1> 
+      </header>
+      <div class="app_question" id="app_question">
+        <p class="owner_question" id="owner_question">What are you looking after with this application?</p>
+        <div class="button_section">
+          <Link to="/register_form"><button class="baby_button"><img class="baby_img"src={baby}/>Human</button></Link>
+          <Link to="/register_form"><button class="dog_button"><img class="dog_img"src={dog}/>Dog</button></Link>
+        </div>
       </div>
     </div>
-  </div>
+  );
 }
 
 export default Welcome;
