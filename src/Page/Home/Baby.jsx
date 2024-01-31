926b054fadc78f2afe70c25073727316cf4c8485
Kanon Inoue <109997852+11186267@users.noreply.github.com>
2024-01-31 21:53:25 +0900

    Ugly signup pages done

--- src/Page/Home/Baby.jsx
+++ src/Page/Home/Baby.jsx
@@ -7,16 +7,80 @@ export default function Baby() {
   const navigateSignUp = () => {
     navigate('/signup');
   }
+
   return (
-    <section>
+    <section id="SignUp" className="sign--section">
+      <div>
+        <h3>Sign Up</h3>
+        <p className="text-lg">
+          Welcome to the app! 
+          <br /> Fill these blanks to keep your data on this application.
+        </p>
+      </div>
+      <form className="sign--form--container">
+        <div className="container">
+          <label htmlFor="username" className="sign--label">
+              <span className="text-md">Your User Name</span>
+              <input 
+                type="text"
+                className="sign--input text-md"
+                name="username"
+                id="username"
+                required />
+          </label>
+          <label htmlFor="babynumber" className="sign--label">
+              <span className="text-md">How Many Baby Do You Have?</span>
+              <input 
+                type="number"
+                className="sign--input text-md"
+                name="babynumber"
+                id="babynumber"
+                required />
+          </label>
+          <label htmlFor="babyname" className="sign--label">
+              <span className="text-md">Your Baby Name</span>
+              <input 
+                type="text"
+                className="sign--input text-md"
+                name="username"
+                id="username"
+                required />
+          </label>
+          <label htmlFor="emial-address" className="sign--label">
+            <span className="text-md">Your Email Address</span>
+            <input 
+              type="text"
+              className="sign--input text-md"
+              name="email-address"
+              id="email-address"
+              required />
+          </label>
+          <label htmlFor="password" className="sign--label">
+            <span className="text-md">Your Password</span>
+            <input 
+              type="text"
+              className="sign--input text-md"
+              name="password"
+              id="password"
+              required />
+          </label>
+          <label htmlFor="passwordagain" className="sign--label">
+            <span className="text-md">Confirm Your Password</span>
+            <input 
+              type="text"
+              className="sign--input text-md"
+              name="passwordagain"
+              id="passwordagain"
+              required />
+          </label>
+        </div>  
+      </form>
       <div>
         <button className="btn btn-primary welcome--btn" onClick={navigateSignUp}>Sign Up</button>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
         </Routes>
-            
       </div>
     </section>
-
   );
 }
--- src/Page/Home/Fish.jsx
+++ src/Page/Home/Fish.jsx
@@ -1,3 +1,77 @@
+import { Routes, Route, useNavigate } from "react-router-dom";
+import SignUp from "./SignUp"
+
 export default function Fish() {
-  return <h2>Fish</h2>;
+  const navigate = useNavigate();
+
+  const navigateSignUp = () => {
+    navigate('/signup');
+  }
+
+  return (
+    <section id="SignUp" className="sign--section">
+      <div>
+        <h3>Sign Up</h3>
+        <p className="text-lg">
+          Welcome to the app! 
+          <br /> Fill these blanks to keep your data on this application.
+        </p>
+      </div>
+      <form className="sign--form--container">
+        <div className="container">
+          <label htmlFor="username" className="sign--label">
+              <span className="text-md">Your User Name</span>
+              <input 
+                type="text"
+                className="sign--input text-md"
+                name="username"
+                id="username"
+                required />
+          </label>
+          <label htmlFor="babynumber" className="sign--label">
+              <span className="text-md">How Many Tanks Do You Have?</span>
+              <input 
+                type="number"
+                className="sign--input text-md"
+                name="babynumber"
+                id="babynumber"
+                required />
+          </label>
+          <label htmlFor="emial-address" className="sign--label">
+            <span className="text-md">Your Email Address</span>
+            <input 
+              type="text"
+              className="sign--input text-md"
+              name="email-address"
+              id="email-address"
+              required />
+          </label>
+          <label htmlFor="password" className="sign--label">
+            <span className="text-md">Your Password</span>
+            <input 
+              type="text"
+              className="sign--input text-md"
+              name="password"
+              id="password"
+              required />
+          </label>
+          <label htmlFor="passwordagain" className="sign--label">
+            <span className="text-md">Confirm Your Password</span>
+            <input 
+              type="text"
+              className="sign--input text-md"
+              name="passwordagain"
+              id="passwordagain"
+              required />
+          </label>
+        </div>  
+      </form>
+      <div>
+        <button className="btn btn-primary welcome--btn" onClick={navigateSignUp}>Sign Up</button>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </section>
+  );
 }
